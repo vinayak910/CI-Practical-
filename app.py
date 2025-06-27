bcd4ed41cb259be1de712afbf7484fc05e71cef8
Vinayak <vinayakchhabra545.vc@gmail.com>
2025-06-27 20:24:14 +0530

    linting error corrected

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 def add(a, b):
-    return a + b 
+    return a + b
 
 
+def sub(a, b):
+    return a - b
 
 
+result1 = add(4, 5)
+result2 = sub(4, 1)
 
-
-
-
-
-
-
-
-
-
-def sub(a , b):return a- b
-
-result1= add(4 , 5)
-
-result2 = sub(4 , 1)
-
-print(result1 , result2)
+print(result1, result2)
